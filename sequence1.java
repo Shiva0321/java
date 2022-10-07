4585dda482d7e3ad614f2312a358329f142b5459
Shiva0321 <113103017+Shiva0321@users.noreply.github.com>
2022-10-08 01:08:45 +0530

    sequence1.java

---
+++ sequence1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Squence1
+{
+
+public static void main(String[] args)
+{
+int n=1,i=3;
+System.out.print(" "+n);
+while(n<=10)
+{
+n=n+i;
+System.out.print(" "+n);
+}
+
+
+}
+}
